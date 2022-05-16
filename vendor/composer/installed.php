58c1a4d9148b4506989f166b798896692e1a3861
Canoine Ethan <ethan.canoine@ynov.com>
2022-05-16 17:03:20 +0200

    Pb dicon

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b7cbb053a03a6d3bdd1f48eba51ad486dc085c0a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b7cbb053a03a6d3bdd1f48eba51ad486dc085c0a',
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.2.0-beta1',
+            'version' => '5.2.0.0-beta1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => 'fca04c07131a81e625a516cf7b98a8c7df0df1c1',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.2.0-beta1',
+            ),
+        ),
+    ),
+);
